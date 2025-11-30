ffe6b0d8413e4e1dc382740b76ead2ff3cc1f129
Roman Meinl <roman-meinl@ukr.net>
2025-11-30 12:39:37 -0200

    /**  * Testing usual components:  * 1. Renders correctly  * 2. Contains expected text, buttons, inputs  * 3. Handles user events like click and input  * 4. Optional: snapshot testing  */

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
-
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [data , setData] = useState(null);
+  const [data, setData] = useState(null);
+  const [toggle, setToggle] = useState(false);
+  const [value, setValue] = useState("");
+
+  const onClick = () => {
+    setToggle((prev) => !prev);
+  };
 
   useEffect(() => {
     setTimeout(() => {
-      setData({})
-    }, 2000)
-  },[])
-
+      setData({});
+    }, 2000);
+  }, []);
 
   return (
     <div>
-      {data && <div>data</div>}
-      <h1>Hello World</h1>
-      <button>Click Me</button>
-      <div>
-        <label>
-          Enter text
-          <input placeholder="Enter the text" type="text" />
-        </label>
+      {toggle && (
+        <div className="toggleApp" data-testid="toggle-app">
+          <h2>TOGGLE CONTAINER</h2>
+        </div>
+      )}
+      <div className="container2">
+        <h3 data-testid="value-input">{value}</h3>
+        {data && <div style={{ color: "orangered" }}>data</div>}
+        <h1>Hello World</h1>
+        <button data-testid="toggle-btn" onClick={onClick}>
+          Click Me
+        </button>
+        <div>
+          <label>
+            Enter text
+            <input
+              onChange={(e) => setValue(e.target.value)}
+              placeholder="Enter the text"
+              type="text"
+            />
+          </label>
+        </div>
       </div>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 describe("TEST-APP", () => {
@@ -8,17 +8,45 @@ describe("TEST-APP", () => {
 
     const HelloWorldElement = screen.getByText(/hello world/i);
     const btn = screen.getByRole("button");
-    const input = screen.getByPlaceholderText(/Enter the text/i);
+
+
+    
     expect(HelloWorldElement).toBeInTheDocument();
     expect(btn).toBeInTheDocument();
-    expect(input).toBeInTheDocument();
- });
-    test("render data", async() => {
-      const { container } = render(<App />);
-      expect(container).toMatchSnapshot();
 
-      const HelloWorldElement = await screen.findByText(/data/i , {} , {timeout: 3000});
-      expect(HelloWorldElement).toBeInTheDocument();
-    });
- 
+    const input = screen.getByPlaceholderText(/Enter the text/i);
+    expect(input).toBeInTheDocument();
+  });
+
+
+  test("render data", async () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+
+    const HelloWorldElement = await screen.findByText(
+      /data/i,
+      {},
+      { timeout: 3000 }
+    );
+    expect(HelloWorldElement).toBeInTheDocument();
+  });
+  test("click-event", async () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+
+    const input = screen.getByPlaceholderText(/Enter the text/i);
+    expect(screen.queryByTestId("value-input")).toContainHTML("");
+    fireEvent.input(input,{
+      target:{value:"12345432"}
+    })
+    expect(screen.queryByTestId("value-input")).toContainHTML("12345432");
+
+    const BTN = screen.getByTestId("toggle-btn");
+
+    expect(screen.queryByTestId("toggle-app")).toBeNull();
+    fireEvent.click(BTN);
+    expect(screen.getByTestId("toggle-app")).toBeInTheDocument();
+    fireEvent.click(BTN);
+    expect(screen.queryByTestId("toggle-app")).toBeNull();
+  });
 });
